0dbbdfc2ac9bb588db7a4fa8706ac7aa4f1dfd51
mahatab <code1@phicode.io>
2023-11-28 17:11:55 +0530

    input experiments

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react'
 
 const User = (props) => {
-const [input, setInput] = useState({
-    name: '',
-    address: '',
-})
+    const [input, setInput] = useState({
+        name: '',
+        address: '',
+    })
 
-   return(
-    <div>
-        <p>Name: {props.name}</p>
-        <p>Role: {props.role}</p>
+    const inputHandlar = e => setInput(prev => ({ ...prev, [e.target.name]: e.target.value }))
+    const showInput = () => console.log(input)
 
-        <h4>Fill up</h4>
-        <input type='text' name='name' value={input.name} onChange={e => setInput(prev => ({...prev, [e.target.name] : e.target.value}))} />
-        <input type='text' name='address' value={input.address} onChange={e => setInput(prev => ({...prev, [e.target.name] : e.target.value}))} />
-    </div>
-   )
+    return (
+        <div>
+            <p>Name: {props.name}</p>
+            <p>Role: {props.role}</p>
+
+            <h4>Fill up</h4>
+            {/* <input type='text' name='name' value={input.name} onChange={e => setInput(prev => ({...prev, [e.target.name] : e.target.value}))} />
+        <input type='text' name='address' value={input.address} onChange={e => setInput(prev => ({...prev, [e.target.name] : e.target.value}))} /> */}
+
+
+            <input type='text' name='address' value={input.address} onChange={inputHandlar} />
+            <button type='button' onClick={showInput}>Show</button>
+        </div>
+    )
 }
 
 export default User
